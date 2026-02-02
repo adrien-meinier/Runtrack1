6cbd811e2d3a2764f9e6703620328958d671e5d2
adrien meinier <adrien.meinier@laplateforme.io>
2026-02-02 13:12:07 +0100

    change

--- job_01.py
+++ job_01.py
@@ -1,5 +1,12 @@
-class Operations:
-    def__init__(self, operator, nombre1, nombre2):
-    self.operator = operator
-    self.nombre1 = nombre1
-    self.nombre2 = nombre2
+class Operation:
+    def __init__(self, nombre1=0, nombre2=0):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+    def __str__(self):
+        return f"Operation(nombre1={self.nombre1}, nombre2={self.nombre2})"
+
+# Instanciation de la classe
+mon_operation = Operation()  # Utilise les valeurs par défaut
+
+print(mon_operation)
