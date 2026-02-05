941ddc2ca7f3562564563d881b6dd3a920d1e456
adrien meinier <adrien.meinier@laplateforme.io>
2026-02-05 15:20:28 +0100

    job04

---
+++ jour4/job04/main1.py
@@ -0,0 +1,5 @@
+from forme import Forme
+from rectangle import Rectangle
+
+rectangle = Rectangle(8, 3)
+print(f"le resultat est: {rectangle.aire()}")
---
+++ jour4/job04/rectangle.py
@@ -0,0 +1,8 @@
+from forme import Forme
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def aire(self):
+        return self.largeur * self.hauteur
